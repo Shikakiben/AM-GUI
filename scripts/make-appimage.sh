3616dd0e80c8c308a505260537639aa47a6fe1f6
Shikakiben <108907095+Shikakiben@users.noreply.github.com>
2026-01-05 19:10:55 +0100

    use specific am-gui binary instead of wildcard

--- scripts/make-appimage.sh
+++ scripts/make-appimage.sh
@@ -8,6 +8,13 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook:fix-namespaces.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export ICON=AM-GUI.png
+export DESKTOP=AM-GUI.desktop
+
+# Deploy dependencies
+quick-sharun  dist/linux-unpacked/am-gui
+
+# Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
