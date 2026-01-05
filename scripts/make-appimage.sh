c75f413f515727306de674e3a0fec7cf7170257f
Shikakiben <108907095+Shikakiben@users.noreply.github.com>
2026-01-05 19:46:24 +0100

    user revert changes

--- scripts/make-appimage.sh
+++ scripts/make-appimage.sh
@@ -14,28 +14,6 @@ export DEPLOY_OPENGL=0
 export DEPLOY_VULKAN=0
 export DEPLOY_P11KIT=0
 
-# Désactive le déploiement des dépendances inutiles par quick-sharun
-export DEPLOY_OPENGL=0
-export DEPLOY_VULKAN=0
-export DEPLOY_P11KIT=0
-export DEPLOY_GTK=0
-export DEPLOY_GDK=0
-export DEPLOY_QT=0
-export DEPLOY_QT_WEB_ENGINE=0
-export DEPLOY_QML=0
-export DEPLOY_SDL=0
-export DEPLOY_GLYCIN=0
-export DEPLOY_IMAGEMAGICK=0
-export DEPLOY_LIBHEIF=0
-export DEPLOY_GEGL=0
-export DEPLOY_BABL=0
-export DEPLOY_PIPEWIRE=0
-export DEPLOY_GSTREAMER=0
-export DEPLOY_GSTREAMER_ALL=0
-export DEPLOY_LOCALE=0
-export DEPLOY_SYS_PYTHON=0
-export DEPLOY_PYTHON=0
-
 # Deploy dependencies
 quick-sharun  ./AppDir/bin/am-gui
 
