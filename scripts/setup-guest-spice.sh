35c6527c0c854f660d442c77288771dabff3eb01
Shikakiben <108907095+Shikakiben@users.noreply.github.com>
2026-01-06 17:45:03 +0100

    Apply local changes: update CI workflow and packaging scripts

--- scripts/setup-guest-spice.sh
+++
@@ -1,1 +0,0 @@
-/tmp/.mount_AM-GUremp10012857443989859772/bin/am-gui: Relink `/usr/lib/libpixman-1.so.0' with `/tmp/.mount_AM-GUremp10012857443989859772/shared/lib/libm.so.6' for IFUNC symbol `ceil'
